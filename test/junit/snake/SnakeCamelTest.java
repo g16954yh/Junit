9feeab5364bf7500f36c1deff626f22705b172dc
g16954yh <g16954yh@s30437.tsuda.ac.jp>
2018-04-26 14:21:30 +0900

    JUnitを利用できるようにしました

---
+++ test/junit/snake/SnakeCamelTest.java
@@ -0,0 +1,14 @@
+package junit.snake;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
