e9785676a5bc9153f7c4913b6d9517dd5c010c48
g16954yh <g16954yh@s20610.tsuda.ac.jp>
2018-05-08 13:49:03 +0900

    テストケースを作成し、エラーを修正しました。

--- test/junit/snake/SnakeCamelTest.java
+++ test/junit/snake/SnakeCamelTest.java
@@ -1,14 +1,28 @@
 package junit.snake;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import snakecamel.SnakeCamelUtil;
+
 public class SnakeCamelTest {
 
+	
 	@Test
-	public void test() {
-		fail("まだ実装されていません");
+	public void snakeToCamelcaseでスネークケースの文字列をキャメルケースに変換できる(){
+		SnakeCamelUtil stu = new SnakeCamelUtil();
+		String expected = "AbcDefGhi";
+		String actual = stu.snakeToCamelcase("abc_def_ghi");
+		assertThat(actual, is(expected));
 	}
 
+	@Test
+	public void camelToSnakecaseでキャメルケースの文字列をスネークケースに変換できる(){
+		SnakeCamelUtil stu = new SnakeCamelUtil();
+		String expected = "abc_def_ghi";
+		String actual = stu.camelToSnakecase("AbcDefGhi");
+		assertThat(actual, is(expected));
+	}
 }
